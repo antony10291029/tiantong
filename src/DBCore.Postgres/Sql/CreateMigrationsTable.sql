e1e2ca6ec15b378704d6193dfce87c80f8be5bdb
zhanglan <zhanglan1315@gmail.com>
2020-02-24 19:33:26 +0800

    fix(DBCore.Postgres): 修复migration脚本问题

--- src/DBCore.Postgres/Sql/CreateMigrationsTable.sql
+++ src/DBCore.Postgres/Sql/CreateMigrationsTable.sql
@@ -1,6 +1,6 @@
 create table if not exists "_migrations" (
   "id" serial not null primary key,
   "batch_id" integer not null,
-  "file_name" TEXT not null,
-  "created_at" TEXT not null
+  "file_name" varchar(255) not null,
+  "created_at" timestamp not null
 );
