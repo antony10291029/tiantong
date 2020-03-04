91de916a51a2789b815a243b4241e475d2f430d1
zhanglan <zhanglan1315@gmail.com>
2020-03-04 11:41:48 +0800

    feat(Renet.Web): 增加Controller.JsonMessage方法

--- src/Renet.Web/Core/Controller.cs
+++ src/Renet.Web/Core/Controller.cs
@@ -5,6 +5,9 @@ namespace Renet.Web
   [ValidateModel]
   public class BaseController : ControllerBase
   {
-
+    public object JsonMessage(string message)
+    {
+      return new { message };
+    }
   }
 }
