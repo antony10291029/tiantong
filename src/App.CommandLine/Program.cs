ac859edeb9b3ca1abbdc7c8b38c7f70c409bbad3
zhanglan <zhanglan1315@gmail.com>
2020-04-29 14:32:35 +0800

    fix(App.CommandLine): 重新配置控制台程序

--- src/App.CommandLine/Program.cs
+++ src/App.CommandLine/Program.cs
@@ -8,19 +8,23 @@ namespace App.CommandLine
     static void Main()
     {
       var plc = new Plc();
+      var random = new Renet.Random();
 
       plc.Name("测试200smart").UseS7200Smart("192.168.20.10", 102);
 
-      plc.State("hb").Int("D1.100").Heartbeat(1).Collect(1);
-      plc.State("scanner").String("D1.120", 10).Collect(1);
+      plc.State("心跳").Int("D1.100").Heartbeat(1000).Collect(1000);
+      plc.State("扫码器").String("D1.120", 10).Collect(500);
 
-      plc.Int("hb").Watch(value => value > 0).Event("event");
-      plc.String("scanner").Watch(value => value != "").Event("scanning");
+      plc.Int("心跳").Watch(value => value > 0).Event("event");
+      plc.String("扫码器").Watch(value => value != "000000000").Event("scanning");
 
-      plc.Int("hb").On("event", value => Console.WriteLine(value));
-      plc.String("scanner").On("scanning", value => {
+      plc.Int("心跳").On("event", value => {
+        Console.WriteLine(value);
+        plc.String("扫码器").Set(random.String(9));
+      });
+      plc.String("扫码器").On("scanning", value => {
         Console.WriteLine("收到数据: " + value);
-        plc.String("scanner").Set("");
+        plc.String("扫码器").Set("000000000");
       });
 
       plc.Run();
