c8fce385fdd98aca6588a5ea19ec8ae0cedddc50
zhanglan <zhanglan1315@gmail.com>
2020-05-07 18:30:05 +0800

    feat(Tiantong.Iot.Client): NotFound Page

--- src/Tiantong.Iot.Client/src/views/_routes.ts
+++ src/Tiantong.Iot.Client/src/views/_routes.ts
@@ -1,9 +1,15 @@
 import Home from './Home/index.vue'
+import NotFound from './_public/NotFound.vue'
 
 export default [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound,
   }
 ]
