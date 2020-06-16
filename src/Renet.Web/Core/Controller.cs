a13678954dfc2b4f31da64e92faeec9925e0227c
zhanglan <zhanglan1315@gmail.com>
2020-06-16 09:30:25 +0800

    feat(Renet.Web): 将 Controller 辅助方法限制为 protected

--- src/Renet.Web/Core/Controller.cs
+++ src/Renet.Web/Core/Controller.cs
@@ -5,32 +5,32 @@ namespace Renet.Web
   [ValidateModel]
   public class BaseController : ControllerBase
   {
-    public object JsonMessage(string message)
+    protected object JsonMessage(string message)
     {
       return new { message };
     }
 
-    public object SuccessOperation(string message)
+    protected object SuccessOperation(string message)
     {
       throw new SuccessOperation(message);
     }
 
-    public object SuccessOperation(object body)
+    protected object SuccessOperation(object body)
     {
       throw new SuccessOperation(body);
     }
 
-    public object SuccessOperation(string message, object id)
+    protected object SuccessOperation(string message, object id)
     {
       throw new SuccessOperation(new { message, id });
     }
 
-    public object FailureOperation(string message)
+    protected object FailureOperation(string message)
     {
       throw new FailureOperation(message);
     }
 
-    public object FailureOperation(object body)
+    protected object FailureOperation(object body)
     {
       throw new FailureOperation(body);
     }
