5cd7f47deb7b195b6433dbd6ad9d1b8f1f1ff1aa
zhanglan1315 <delylaric@gmail.com>
2020-09-15 22:31:16 +0800

    feat(Namei.Wcs): 封装 WMS 接口

--- src/Namei.Wcs/Namei.Wcs.Api/src/Services/WmsService.cs
+++ src/Namei.Wcs/Namei.Wcs.Api/src/Services/WmsService.cs
@@ -1,4 +1,10 @@
 using DotNetCore.CAP;
+using System;
+using System.Net.Mime;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace Namei.Wcs.Api
 {
@@ -7,29 +13,66 @@ namespace Namei.Wcs.Api
     public string Destination { get; set; } = "2";
 
     public string TaskId { get; set; } = "0001";
+
+    public string Barcode { get; set; }
   }
 
   public class WmsService
   {
     public static string Destination = "1";
 
-    public ICapPublisher _cap;
+    private ICapPublisher _cap;
 
-    public WmsService(ICapPublisher cap)
+    private HttpClient _client;
+
+    public WmsService(ICapPublisher cap, IHttpClientFactory factory)
     {
       _cap = cap;
+      _client = factory.CreateClient();
+      _client.Timeout = new TimeSpan(0, 0, 10);
+      _client.BaseAddress = new System.Uri("http://172.16.2.52:8086");
+      _client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json)
+      );
     }
 
     public PalletInfo GetPalletInfo(string barcode)
     {
+      var json = JsonSerializer.Serialize(new {
+        method = "ReqTask",
+        BARCODE = barcode,
+      });
+      var content = new StringContent(json, Encoding.UTF8);
+      var response = _client.PostAsync("/namei_wms/wcsCallback/request", content).GetAwaiter().GetResult();
+      var result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+      var dom = JsonDocument.Parse(response.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+      var taskid = dom.RootElement.GetProperty("taskid").GetString();
+      var destination =  dom.RootElement.GetProperty("TO_LF").GetString();
+
+      if (taskid == "") {
+        throw new Exception("任务查询失败");
+      }
+
       return new PalletInfo() {
-        Destination = Destination,
+        Barcode = barcode,
+        Destination = destination,
+        TaskId = taskid,
       };
     }
 
-    public void RequestPicking(string lifterId, string floor, string code, string taskId)
+    public void RequestPicking(string lifterId, string floor, string barcode, string taskId)
     {
+      var json = JsonSerializer.Serialize(new {
+        method = "FinishReport",
+        LF_NO = $"LF0{lifterId}",
+        TO_LF = $"LF0{floor}",
+        taskid = taskId,
+        BARCODE = barcode,
+      });
 
+      var content = new StringContent(json, Encoding.UTF8);
+      var response = _client.PostAsync("/namei_wms/wcsCallback/request", content).GetAwaiter().GetResult();
+      var result = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
     }
   }
 }
