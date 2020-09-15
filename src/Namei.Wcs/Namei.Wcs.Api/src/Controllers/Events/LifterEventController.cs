7bc945a3727c65c3be4eb347007dfb33cce8e299
zhanglan1315 <delylaric@gmail.com>
2020-09-15 22:23:55 +0800

    fix(Namei.Wcs): 修复请求取货逻辑

--- src/Namei.Wcs/Namei.Wcs.Api/src/Controllers/Events/LifterEventController.cs
+++ src/Namei.Wcs/Namei.Wcs.Api/src/Controllers/Events/LifterEventController.cs
@@ -1,6 +1,5 @@
 using DotNetCore.CAP;
 using Renet.Web;
-using System.Threading.Tasks;
 
 namespace Namei.Wcs.Api
 {
@@ -40,7 +39,14 @@ namespace Namei.Wcs.Api
     public void HandleTaskScanned(LifterTaskScannedEvent param)
     {
       var barcode = _lifters.Get(param.LifterId).GetPalletCode(param.Floor);
-      var destination = _wms.GetPalletInfo(barcode).Destination;
+      var destination = "";
+
+      try {
+        destination = _wms.GetPalletInfo(barcode).Destination;
+      } catch {
+        _cap.Publish(LifterTaskQueryFailedEvent.Message, new LifterTaskQueryFailedEvent(param.LifterId, param.Floor, barcode));
+        return;
+      }
 
       // 目标楼层与当前楼层相同，则不触发 TaskQueried 事件
       if (param.Floor == destination) {
@@ -62,21 +68,20 @@ namespace Namei.Wcs.Api
     public void HandleTaskExported(LifterTaskExportedEvent param)
     {
       var barcode = _lifters.Get(param.LifterId).GetPalletCode(param.Floor);
-      var taskId = _wms.GetPalletInfo(barcode).TaskId;
 
-      _wms.RequestPicking(param.LifterId, param.Floor, barcode, taskId);
-      _cap.Publish(LifterTaskPickingEvent.Message, new LifterTaskPickingEvent(param.LifterId, param.Floor, barcode));
+      try {
+        var taskId = _wms.GetPalletInfo(barcode).TaskId;
+        _wms.RequestPicking(param.LifterId, param.Floor, barcode, taskId);
+        _cap.Publish(LifterTaskPickingEvent.Message, new LifterTaskPickingEvent(param.LifterId, param.Floor, barcode, param.TaskId));
+      } catch {
+        _cap.Publish(LifterTaskPickingFailedEvent.Message, new LifterTaskPickingFailedEvent(param.LifterId, param.Floor, barcode));
+      }
     }
 
     [CapSubscribe(LifterTaskTakenEvent.Message, Group = Group)]
     public void HandleTaskTaken(LifterTaskTakenEvent param)
     {
       _lifters.Get(param.LifterId).SetPickuped(param.Floor, true);
-
-      Task.Delay(1000).ContinueWith(async task => {
-        await task;
-        _lifters.Get(param.LifterId).SetPickuped(param.Floor, false);
-      });
     }
   }
 }
