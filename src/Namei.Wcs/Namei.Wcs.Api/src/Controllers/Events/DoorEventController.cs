331bac4f2c393fcd7aeb71efe17ee1a10e0d99ed
zhanglan1315 <delylaric@gmail.com>
2020-09-15 22:22:50 +0800

    fix(Namei.Wcs): 重构自动门事件控制器

---
+++ src/Namei.Wcs/Namei.Wcs.Api/src/Controllers/Events/DoorEventController.cs
@@ -0,0 +1,51 @@
+using DotNetCore.CAP;
+using Renet.Web;
+
+namespace Namei.Wcs.Api
+{
+  public class DoorEventController: BaseController
+  {
+    const string Group = "command";
+
+    private DoorServiceManager _doors;
+
+    public DoorEventController(
+      DomainContext domain,
+      DoorServiceManager doors,
+      DoorTaskManager taskManager,
+      RcsService rcs
+    ) {
+      _doors = doors;
+    }
+
+    [CapSubscribe(DoorOpenedEvent.Message, Group = Group)]
+    public void HandleDoorOpened(DoorOpenedEvent param)
+    {
+      _doors.Get(param.DoorId).OnOpened();
+    }
+
+    [CapSubscribe(DoorClosedEvent.Message, Group = Group)]
+    public void HandleDoorClosed(DoorClosedEvent param)
+    {
+      _doors.Get(param.DoorId).OnClosed();
+    }
+
+    // 放货完成
+    [CapSubscribe(LifterTaskImportedEvent.Message, Group = Group)]
+    public void HandleLifterTaskImported(LifterTaskImportedEvent param)
+    {
+      var doorId = CrashDoor.GetDoorIdFromLifter(param.Floor, param.LifterId);
+
+      _doors.Get(doorId).Open();
+    }
+
+    // 取货完成
+    [CapSubscribe(LifterTaskTakenEvent.Message, Group = Group)]
+    public void HandleLifterTaskTaken(LifterTaskTakenEvent param)
+    {
+      var doorId = CrashDoor.GetDoorIdFromLifter(param.Floor, param.LifterId);
+
+      _doors.Get(doorId).Open();
+    }
+  }
+}
