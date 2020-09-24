3498bff29064f2ed4ca1f5277dbe8e40fc188a02
zhanglan1315 <delylaric@gmail.com>
2020-09-24 15:58:36 +0800

    refact(Namei.Wcs): 日志查询接口重构

--- src/Namei.Wcs/Namei.Wcs.Api/src/Controllers/LoggerController.cs
+++ src/Namei.Wcs/Namei.Wcs.Api/src/Controllers/LoggerController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Renet;
 using Renet.Web;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Namei.Wcs.Api
 {
@@ -16,7 +17,9 @@ namespace Namei.Wcs.Api
 
     public class SearchParams
     {
-      public string search { get; set; }
+      public List<string> search { get; set; } = new List<string>();
+
+      public List<string> query { get; set; } = new List<string>();
 
       public int page { get; set; }
 
@@ -27,7 +30,40 @@ namespace Namei.Wcs.Api
     [Route("/logs/search")]
     public object Search([FromBody] SearchParams param)
     {
-      return _domain.Logs.Where(log => log.key.StartsWith(param.search))
+      var search = param.search;
+      var searchCount = search.Count;
+      var query = param.query;
+      var queryCount = query.Count;
+
+      if (searchCount > 10 || queryCount > 10) {
+        throw KnownException.Error("查询参数超过最多为 10 个");
+      }
+
+      return _domain.Logs
+        .Where(log =>
+          (0 < searchCount ? log.key.Contains(search[0]) : true) &&
+          (1 < searchCount ? log.key.Contains(search[1]) : true) &&
+          (2 < searchCount ? log.key.Contains(search[2]) : true) &&
+          (3 < searchCount ? log.key.Contains(search[3]) : true) &&
+          (4 < searchCount ? log.key.Contains(search[4]) : true) &&
+          (5 < searchCount ? log.key.Contains(search[5]) : true) &&
+          (6 < searchCount ? log.key.Contains(search[6]) : true) &&
+          (7 < searchCount ? log.key.Contains(search[7]) : true) &&
+          (8 < searchCount ? log.key.Contains(search[8]) : true) &&
+          (9 < searchCount ? log.key.Contains(search[9]) : true) &&
+          (10 < searchCount ? log.key.Contains(search[10]) : true) &&
+          (0 < queryCount ? log.message.Contains(query[0]) : true) &&
+          (1 < queryCount ? log.message.Contains(query[1]) : true) &&
+          (2 < queryCount ? log.message.Contains(query[2]) : true) &&
+          (3 < queryCount ? log.message.Contains(query[3]) : true) &&
+          (4 < queryCount ? log.message.Contains(query[4]) : true) &&
+          (5 < queryCount ? log.message.Contains(query[5]) : true) &&
+          (6 < queryCount ? log.message.Contains(query[6]) : true) &&
+          (7 < queryCount ? log.message.Contains(query[7]) : true) &&
+          (8 < queryCount ? log.message.Contains(query[8]) : true) &&
+          (9 < queryCount ? log.message.Contains(query[9]) : true) &&
+          (10 < queryCount ? log.message.Contains(query[10]) : true)
+        )
         .OrderByDescending(log => log.created_at)
         .Paginate(param.page, param.page_size);
     }
