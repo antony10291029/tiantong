4fabdacac226afb0ec32d3a706b7ca25e2e4e3d7
zhanglan <zhanglan1315@gmail.com>
2021-02-18 01:57:03 +0800

    refact(Midos.Iot.Client): Clear Logs

---
+++ projects/Midos.Iot/Midos.Iot.Client/src/views/Plcs.Dashboard/ClearLogs.vue
@@ -0,0 +1,83 @@
+<template>
+  <div style="padding: 1.25rem">
+    <p class="title is-5">
+      日志清理
+    </p>
+
+    <p class="content">
+      日志数据量过大将影响系统性能，若有需要可根据以下条件清理进行清理。
+    </p>
+
+    <div class="is-flex">
+      <label
+        class="label"
+        @click="days = 30"
+      >
+        <Radio :value="days === 30"></Radio>
+        保留 30 天
+      </label>
+
+      <div style="width: 1rem"></div>
+
+      <label
+        class="label"
+        @click="days = 7"
+      >
+        <Radio :value="days === 7"></Radio>
+        保留 7 天
+      </label>
+
+      <div style="width: 1rem"></div>
+
+      <label
+        class="label"
+        @click="days = 0"
+      >
+        <Radio :value="days === 0"></Radio>
+        清理全部
+      </label>
+    </div>
+
+    <div style="height: 1.25rem"></div>
+
+    <div class="is-flex is-vcentered">
+      <AsyncButton
+        :handler="handleDelete"
+        class="button is-info is-light is-small"
+      >
+        开始清理
+      </AsyncButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useConfirm } from "@/share/hooks/use-confirm";
+import { useIotHttp } from "@/services/iot-http-client";
+
+export default defineComponent({
+  name: "ClearLogs",
+
+  setup() {
+    const http = useIotHttp();
+    const confirm = useConfirm();
+    const days = ref(30);
+
+    function handleDelete () {
+      confirm.open({
+        title: "提示",
+        content: "该操作将删除日志，请确认",
+        handler: async () => {
+          await http.post("/plc-logs/clear", { days: days.value });
+        }
+      });
+    }
+
+    return {
+      days,
+      handleDelete
+    };
+  }
+});
+</script>
--- projects/Midos.Iot/Midos.Iot.Client/src/views/Plcs.Dashboard/index.vue
+++ projects/Midos.Iot/Midos.Iot.Client/src/views/Plcs.Dashboard/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div style="padding: 1.25rem">
-    <div class="box is-paddingless">
-      <div
-        style="border-bottom: none"
-      >
+    <div class="box" style="padding: 0.5rem 0">
+      <div>
         <QuickOperations />
       </div>
 
@@ -11,6 +9,12 @@
 
       <div>
         <Autorun />
+      </div>
+
+      <hr style="margin: 1.25rem">
+
+      <div>
+        <ClearLogs />
       </div>
     </div>
   </div>
@@ -20,12 +24,14 @@
 import { defineComponent } from "vue";
 import QuickOperations from "./QuickOperations.vue";
 import Autorun from "./Autorun.vue";
+import ClearLogs from "./ClearLogs.vue";
 
 export default defineComponent({
   name: "PlcsDashboard",
 
   components: {
     Autorun,
+    ClearLogs,
     QuickOperations
   }
 });
