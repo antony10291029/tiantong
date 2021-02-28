05c17519ae1451e2c9de8961007c8ef56a85d365
zhanglan <zhanglan1315@gmail.com>
2021-03-01 00:37:38 +0800

    feat(Namei.Midos.Center.Api): 初始化 Midos.Center.Api 项目

---
+++ projects/Namei/Namei.Midos.Center.Api/Program.cs
@@ -0,0 +1,13 @@
+using MidosProgram = Midos.Center.Program;
+
+namespace Namei.Midos.Server
+{
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      MidosProgram.Main(args);
+    }
+  }
+}
