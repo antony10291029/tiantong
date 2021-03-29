08a7da7ca79c3706a1770fd2b44c36d211662efc
zhanglan1315 <delylaric@gmail.com>
2021-03-29 14:39:16 +0800

    chore(Namei.Wcs.Api.Test): 成立 Wcs 测试项目

---
+++ tests/Namei.Wcs.Api.Test/TestService.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Namei.Wcs.Api.Test
+{
+  [TestFixture]
+  public class TestService
+  {
+    [TestCase]
+    public void Test()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
