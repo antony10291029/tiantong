474ffa2b774d9922bdb9012654ea893d5a7e2c8d
zhanglan1315 <delylaric@gmail.com>
2021-03-29 11:30:03 +0800

    refact(Namei.Wcs.Api): 移除提升机测试指令 Api

--- projects/Namei/Namei.Wcs.Api/src/Controllers/TestServiceController.cs
+++ projects/Namei/Namei.Wcs.Api/src/Controllers/TestServiceController.cs
@@ -15,55 +15,6 @@ namespace Namei.Wcs.Api
       _lifters = lifters;
     }
 
-    [HttpPost]
-    [Route("/test/lifters/destination")]
-    public object GetDestination()
-    {
-      return new {
-        destination = WmsService.Destination,
-      };
-    }
-
-    public class SetDestinationParams
-    {
-      public string destination { get; set; }
-    }
-
-    [HttpPost]
-    [Route("/test/lifters/set-destination")]
-    public object SetDestination([FromBody] SetDestinationParams param)
-    {
-      WmsService.Destination = param.destination;
-
-      return NotifyResult.FromVoid().Success("目标楼层已设置");
-    }
-
-    public class LifterNotifyParams
-    {
-      public string lifter_id { get; set; }
-
-      public string floor { get; set; }
-
-      public string message { get; set; }
-    }
-
-    [HttpPost]
-    [Route("/test/lifters/publish-message")]
-    public object PublishMessage([FromBody] LifterNotifyParams param)
-    {
-      if (param.message == "imported") {
-        _lifters.Get(param.lifter_id).SetImported(param.floor, true);
-      } else if (param.message == "exported") {
-        _cap.Publish(LifterTaskExported.Message, LifterTaskExported.From(param.lifter_id, param.floor));
-      } else if (param.message == "scanned") {
-        _cap.Publish(LifterTaskScannedEvent.Message, new LifterTaskScannedEvent(param.lifter_id, param.floor));
-      } else if (param.message == "taken") {
-        _lifters.Get(param.lifter_id).SetPickuped(param.floor, true);
-      }
-
-      return NotifyResult.FromVoid().Success($"指令已发送: {param.lifter_id} 号梯，{param.floor} 楼");
-    }
-
     public class PublishDoorsMessageParams
     {
       public string door_id { get; set; }
