0070aaa72149c372afbb4a59798c780bbcb86845
zhanglan <zhanglan1315@gmail.com>
2021-04-01 15:30:26 +0800

    fix(Midos.Center): 修复 SubtaskOrders 未设主键的问题

--- projects/Midos.Center/Midos.Center.Database/Sql/000410_CreateSubtaskOrdersTable.sql
+++ projects/Midos.Center/Midos.Center.Database/Sql/000410_CreateSubtaskOrdersTable.sql
@@ -1,5 +1,5 @@
 create table if not exists "SubtaskOrders" (
-  "Id" bigserial not null,
+  "Id" bigserial not null primary key,
   "SubtypeId" bigint not null,
   "OrderId" bigint not null,
   "SuborderId" bigint not null
