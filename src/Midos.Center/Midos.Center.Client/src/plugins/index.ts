0ee85d65475abecc9ec48dcb7ee91a8970f8ec65
zhanglan <zhanglan1315@gmail.com>
2021-05-06 12:58:04 +0800

    chore(Namei.Wcs): 将 DataMapIterator 移至 @midos/vue-ui 中

--- src/Midos.Center/Midos.Center.Client/src/plugins/index.ts
+++ src/Midos.Center/Midos.Center.Client/src/plugins/index.ts
@@ -1,8 +1,7 @@
 import { Plugin } from "vue";
-import DataMapIterator from "./DataMapIterator.vue";
 
 export default {
   install(app) {
-    app.component("DataMapIterator", DataMapIterator);
+
   }
 } as Plugin;
