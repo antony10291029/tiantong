fd952c566ff3102d66be4217a4d72c352c5c35db
zhanglan <zhanglan1315@gmail.com>
2021-05-17 14:22:20 +0800

    feat(Namei.Midos.Client): 更新前端架构

--- src/Namei/Namei.Midos.Client/index.ts
+++
@@ -1,2 +0,0 @@
-export { NameiWcs } from "./src/app-namei-wcs";
-export { NameiWcsHttp, useWcsHttp } from "./src/services/wcs-http";
--- src/Namei/Namei.Midos.Client/src/main.ts
+++ src/Namei/Namei.Midos.Client/src/main.ts
@@ -1,10 +1,11 @@
 /* eslint-disable indent */
 import { MidOS } from "@midos/core";
+import { DomainSeedWork } from "@midos/seed-work";
 import { VueUI, Confirm, Notify, HttpNotifyMiddleware, Config, VueEnv } from "@midos/vue-ui";
 import { MidosCenter, MidosCenterHttp } from "@midos/app-midos-center";
 import { NameiWcs, NameiWcsHttp } from "@midos/app-namei-wcs";
 import { AppIot, IotHttpClient } from "@midos/app-iot";
-import { ApiGateway, ApiGatewayHttp } from "@midos/app-api-proxy";
+import { ApiGateway, ApiGatewayHttp, RouteRepository, EndpointRepository } from "@midos/app-api-gateway";
 import "@midos/vue-ui/style.sass";
 
 MidOS.create()
@@ -13,6 +14,7 @@ MidOS.create()
     .singleton(Confirm)
     .singleton(Notify)
     .singleton(VueEnv)
+  .useSeedWork(DomainSeedWork)
   .addHttpMiddleware(HttpNotifyMiddleware)
   .addApp(NameiWcs)
     .addHttpClient(NameiWcsHttp)
@@ -22,5 +24,7 @@ MidOS.create()
     .addHttpClient(IotHttpClient)
   .addApp(MidosCenter)
     .addHttpClient(MidosCenterHttp)
+    .singleton(RouteRepository)
+    .singleton(EndpointRepository)
   .build()
   .run();
