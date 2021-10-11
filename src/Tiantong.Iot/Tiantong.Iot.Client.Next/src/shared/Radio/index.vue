171d6116f79b103fc417c3b265f59e865207c138
zhanglan <zhanglan1315@gmail.com>
2021-10-11 10:33:22 +0800

    feat(Tiantong.Iot.Api): 实现 Radio Component

---
+++ src/Tiantong.Iot/Tiantong.Iot.Client.Next/src/shared/Radio/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <i
+    v-if="value"
+    @click="handleUpdate(false)"
+    class="iconfont icon-radio-checked cursor-pointer text-link-500"
+  ></i>
+  <i
+    v-else
+    @click="handleUpdate(true)"
+    class="iconfont icon-radio-unchecked cursor-pointer text-dark-500"
+  ></i>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  emits: [
+    "update:value"
+  ],
+
+  setup(props, { emit }) {
+    const handleUpdate = (value: boolean) => {
+      emit("update:value", value);
+    };
+
+    return {
+      handleUpdate
+    };
+  }
+});
+</script>
