b45702a8419885ee2efb98f3ac472f04fb2a0002
zhanglan <zhanglan1315@gmail.com>
2021-10-14 12:29:24 +0800

    fix(Tiantong.Iot): 修复 TcpClient 重连问题

--- packages/Midos/Midos.Net/RenetTcpClient.cs
+++ packages/Midos/Midos.Net/RenetTcpClient.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace System.Net.Sockets
 {
   public class RenetTcpClient: IDisposable
@@ -6,9 +9,11 @@ namespace System.Net.Sockets
 
     public string Host;
 
+    private Stream _stream;
+
     private TcpClient _client;
 
-    private int _ioTimeout = 10000;
+    private int _ioTimeout = 3000;
 
     private int BufferLength = 1024;
 
@@ -24,12 +29,14 @@ namespace System.Net.Sockets
 
     public void Dispose()
     {
-      _client.Dispose();
+      _client?.Dispose();
+      _stream?.Dispose();
     }
 
     public void Close()
     {
-      _client.Close();
+      _client?.Close();
+      _stream?.Close();
     }
 
     public byte[] Send(byte[] message)
@@ -44,11 +51,11 @@ namespace System.Net.Sockets
     private void Send(byte[] message, byte[] buffer)
     {
       lock (_sendingLock) {
-        if (_client.GetStream().CanWrite) {
-          _client.GetStream().Write(message, 0, message.Length);
+        if (_stream.CanWrite) {
+          _stream.Write(message, 0, message.Length);
         }
-        if (_client.GetStream().CanRead) {
-          _client.GetStream().Read(buffer, 0, buffer.Length);
+        if (_stream.CanRead) {
+          _stream.Read(buffer, 0, buffer.Length);
         }
       }
     }
@@ -56,11 +63,11 @@ namespace System.Net.Sockets
     public void Connect()
     {
       _client = new TcpClient();
-      _client.SendBufferSize = 5120;
-      _client.ReceiveBufferSize = 5120;
-      _client.SendTimeout = _client.ReceiveTimeout = _ioTimeout;
-
+      _client.SendBufferSize = 512;
+      _client.ReceiveBufferSize = 512;
       if (_client.ConnectAsync(Host, Port).Wait(_ioTimeout)) {
+        _stream = _client.GetStream();
+        _stream.ReadTimeout = _stream.WriteTimeout = _ioTimeout;
         Connected();
       } else {
         throw KnownException.Error("网络连接超时");
